5c9cdca4dfc17934af4cf058d2df3a15098cf294
Qoussay <qoussay.akkari@gmail.com>
2024-01-18 12:43:13 +0100

    added photo grid to the about page

--- src/lib/pageSections/about/Hero.svelte
+++ src/lib/pageSections/about/Hero.svelte
@@ -2,12 +2,11 @@
 
 <section id="AboutHero">
 	<!-- images  -->
-	<div class="grid grid-cols-2 md:grid-cols-3 max-w-[100rem] mx-auto px-4 sm:px-8">
-		<h1>hey</h1>
-		<h1>hey</h1>
-		<h1>hey</h1>
-		<h1>hey</h1>
-		<h1>hey</h1>
-		<h1>hey</h1>
+	<div class="grid grid-cols-2 md:grid-cols-3 max-w-[100rem] mx-auto px-8 sm:px-12 gap-4">
+		<img class="order-1" src="/images/about1.png" alt="about one" />
+		<img class="col-span-2 order-3 md:order-2" src="/images/about2.png" alt="about two" />
+		<img class="order-2 md:order-3" src="/images/about3.png" alt="about three" />
+		<img class="order-4" src="/images/about4.png" alt="about four" />
+		<img class="order-5" src="/images/about5.png" alt="about five" />
 	</div>
 </section>
