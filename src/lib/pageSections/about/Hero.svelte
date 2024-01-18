9525bf1214ea77e92d8aaf521c0118b8e2108086
Qoussay <qoussay.akkari@gmail.com>
2024-01-18 14:21:01 +0100

    added hero section to the about page

--- src/lib/pageSections/about/Hero.svelte
+++ src/lib/pageSections/about/Hero.svelte
@@ -1,12 +1,47 @@
 <script></script>
 
-<section id="AboutHero">
+<section id="AboutHero" class="py-8">
+	<!-- title and description  -->
+	<div class="text-center max-w-[40rem] mx-auto px-4 sm:px-12">
+		<h1 class="font-bold text-secondary text-5xl/tight lg:text-6xl/tight py-8">Why RA Global</h1>
+		<p class="font-medium text-base md:text-lg xl:text-xl">
+			We redefine education through personalized scholarship opportunities, inclusive communities,
+			and unwavering commitment to academic success. We stand as your trusted partner, unlocking
+			transformative educational experiences with precision and care.
+		</p>
+	</div>
 	<!-- images  -->
-	<div class="grid grid-cols-2 md:grid-cols-3 max-w-[100rem] mx-auto px-8 sm:px-12 gap-4">
-		<img class="order-1" src="/images/about1.png" alt="about one" />
-		<img class="col-span-2 order-3 md:order-2" src="/images/about2.png" alt="about two" />
-		<img class="order-2 md:order-3" src="/images/about3.png" alt="about three" />
-		<img class="order-4" src="/images/about4.png" alt="about four" />
-		<img class="order-5" src="/images/about5.png" alt="about five" />
+	<div class="grid grid-cols-2 md:grid-cols-3 max-w-[100rem] mx-auto px-4 sm:px-12 gap-4 py-24">
+		<img class="w-full h-full order-1" src="/images/about1.png" alt="about one" />
+		<img
+			class="w-full h-full col-span-2 order-3 md:order-2"
+			src="/images/about2.png"
+			alt="about two"
+		/>
+		<img class="w-full h-full order-2 md:order-3" src="/images/about3.png" alt="about three" />
+		<img class="w-full h-full order-4" src="/images/about4.png" alt="about four" />
+		<img class="w-full h-full order-5" src="/images/about5.png" alt="about five" />
+	</div>
+	<!-- detailed description -->
+	<div
+		class="flex flex-col space-y-12 font-medium text-base/relaxed md:text-lg/loose max-w-[55rem] mx-auto px-4 sm:px-12 pb-12"
+	>
+		<p>
+			At RA Global, we prioritize students' needs. Founded by former students who understand the
+			pitfalls of education agencies in North Africa, we're here to revolutionize. Our services go
+			beyond consultation; they're a commitment to ensuring every student gets the best education.
+			We make education accessible—our services are entirely free. Having experienced industry
+			indifference, we're here to change the narrative. As students advocating for students, we
+			align our mission with yours – securing the best educational opportunities without compromise.
+		</p>
+		<p>
+			At RA Global, we navigate education consulting to empower students. Our partnership with
+			esteemed universities is a testament to our commitment to excellence. We exclusively
+			collaborate with institutions that share our vision of fostering academic success. What sets
+			us apart is our dedication to securing up to 100% scholarships. It's about transforming dreams
+			into realities. Our founders' journey fuels our mission – to redefine education consultancy in
+			North Africa. True guidance is a promise of a brighter future. Choose RA Global, where your
+			educational aspirations become our collective priority.
+		</p>
 	</div>
 </section>
