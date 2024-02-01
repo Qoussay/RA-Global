889ed59ce64c906a3d5134bb014aad8284ee4615
Qoussay <qoussay.akkari@gmail.com>
2024-02-01 16:08:31 +0100

    trying AJAX form submission on netlify

--- src/routes/contact/+server.js
+++
@@ -1,9 +0,0 @@
-export async function POST({ request }) {
-	console.log('POST request received');
-	console.log(request);
-}
-
-export async function GET({ request }) {
-	console.log('GET request received');
-	console.log(request);
-}
