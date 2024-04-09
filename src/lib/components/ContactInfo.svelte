f945258779eba24e6569583112277dd8bc617dcf
Qoussay <qoussay.akkari@gmail.com>
2024-04-09 03:30:36 +0100

    small fixed

--- src/lib/components/ContactInfo.svelte
+++ src/lib/components/ContactInfo.svelte
@@ -9,13 +9,16 @@
 		<MailIcon class="w-10 xl:w-11 h-10 xl:h-11 p-3 rounded-2xl bg-primary text-white my-auto" />
 		<div>
 			<p class="text-sm xl:text-base font-bold text-primary">Email</p>
-			<p class="text-sm xl:text-base font-medium">info@raglobal.tn</p>
+			<a href="mailto:info@raglobal.tn" class="text-sm xl:text-base font-medium">info@raglobal.tn</a
+			>
 		</div>
 	{:else if type === 'phone'}
 		<PhoneIcon class="w-10 xl:w-11 h-10 xl:h-11 p-3 rounded-2xl bg-secondary text-white my-auto" />
 		<div>
-			<p class="text-sm xl:text-base font-bold text-secondary">Phone</p>
-			<p class="text-sm xl:text-base font-medium">+216-48 063 544</p>
+			<p class="text-sm xl:text-base font-bold text-secondary">Whatsapp</p>
+			<a href="https://wa.me/+21648063544" class="text-sm xl:text-base font-medium"
+				>+216-48 063 544</a
+			>
 		</div>
 	{/if}
 </div>
